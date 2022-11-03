d38455e7e5955688fbf4be504179db1f9673c609
Charles Matthews <charles0matthews@gmail.com>
2022-11-03 16:16:10 -0400

    progressing basic test

--- index.js
+++ index.js
@@ -5,12 +5,23 @@
  */
 
 const KeyboardMapper = function(keymap){
-    
+    this.defaultFunction = 'test';
+    this.keymap_init();
+    console.log(this.keymap)
 }
 
 KeyboardMapper.prototype = {}
 
 KeyboardMapper.prototype.constructor = KeyboardMapper;
+
+KeyboardMapper.prototype.default = function(e,item){
+    console.log('hello tgere',item,this.defaultFunction)
+}
+
+KeyboardMapper.prototype.keymap_init = function(){
+    // 
+    Object.keys(this.keymap).forEach(key=>{this.keymap[key] = this.default},this)
+}
 
 /**
  * Common keystrokes to map
@@ -21,6 +32,11 @@ KeyboardMapper.prototype.keymap = {
     Arrow:          {},
     Digit:          {},
     F:              {},
+
+    Meta:           {},
+    Alt:            {},
+    Shift:          {},
+    Control:        {},
     // end sequences / prefixes
 
     Backspace:      {},
@@ -47,7 +63,11 @@ KeyboardMapper.prototype.sequences = {
     Key:    ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
     Arrow:  ['Left','Right','Up','Down'],
     Digit:  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    F:      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    F:      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    Meta:   ['Left','Right'],
+    Alt:    ['Left','Right'],
+    Shift:  ['Left','Right'],
+    Control:['Left','Right']
 }
 
 /**
@@ -57,11 +77,17 @@ KeyboardMapper.prototype.sequences = {
 
 KeyboardMapper.prototype.process = function(e){
 
-    // keycode = '', repeat = false, metaKey = false, shiftKey = false
+    // code = '', repeat = false, metaKey = false, shiftKey = false
 
+    // console.log(e)
+    let category = Object.keys(this.keymap).find((item)=>e.code.includes(item))
+    let item = e.code.replace(category,'')
+    // console.log(category, item);
+    this.keymap[category](e,item);
     // detect prefix
     // slice string based on length of prefix
     // run function
+    
 }
 
 module.exports = KeyboardMapper;
---
+++ test/test.js
@@ -0,0 +1,2 @@
+document.addEventListener('keydown',(e)=>{keyboardMapper.process(e)})
+document.addEventListener('keyup',(e)=>{keyboardMapper.process(e)})
