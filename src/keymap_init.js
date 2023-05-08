f09a9f8a694bfd4115cc459978f97df92c6b7713
Charles Matthews <charles0matthews@gmail.com>
2023-05-08 08:53:50 -0400

    Update keymap_init.js

--- src/keymap_init.js
+++ src/keymap_init.js
@@ -1,7 +1,9 @@
 const keymap_init = function(){
     Object.keys(this.keymap).forEach(key=>{this.keymap[key] = this.default},this)
     Object.keys(this.ranges).forEach(key=>{
-        this.keyranges[key]={}
+        this.keyranges[key]={};
+        this.keyUp[key] = ()=>{};
+        this.keyDown[key] = ()=>{};    
         this.ranges[key].forEach(x=>{
             // was this.default, but like this, if typeof item != undefined then override?
             this.keyranges[key][x.toString()] = undefined
@@ -10,3 +12,4 @@ const keymap_init = function(){
 }
 
 export default keymap_init
+
