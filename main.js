2783df3cd987ee12c96b2ca6368bf02056cb72c9
Charles Matthews <charles0matthews@gmail.com>
2023-11-08 22:00:59 -0500

    test file

---
+++ main.js
@@ -0,0 +1,2 @@
+import {KeyboardMapper} from './dist/keyboard-mapper.es.js';
+window.keyboardMapper = new KeyboardMapper(false);
--- test_src/test.js
+++
@@ -1,2 +0,0 @@
-import KeyboardMapper from '../index.js';
-window.keyboardMapper = new KeyboardMapper(false);
